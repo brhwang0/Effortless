ab9b304ee6c673f11f8ee2fcafe1758183bfc3a9
brhwang0 <brhwang@protonmail.com>
2020-01-01 22:14:35 -0500

    Rework slideshow div, change slideshow based on time interval

--- src/app/slideshow/slideshow.component.ts
+++ src/app/slideshow/slideshow.component.ts
@@ -10,6 +10,32 @@ export class SlideshowComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    var slideIndex = 1;
+    showSlides(slideIndex);
+
+    function showSlides(n) {
+      setTimeout(showSlides, 2000); // Change image every 2 seconds
+      var i;
+      var slides = document.getElementsByClassName("slide");
+
+      if (slideIndex > slides.length) {
+        slideIndex = 1;
+      }
+
+      if (slideIndex < 1) {
+        slideIndex = slides.length
+      }
+
+      for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+      }
+
+      slides[slideIndex-1].style.display = "flex";
+      slideIndex++;
+    }
+
   }
+
+
 
 }
