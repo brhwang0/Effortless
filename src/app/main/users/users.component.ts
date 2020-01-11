d68de2e4528192290473fce0116a4410cec33a2a
RachelTan07 <Rachel.Tan07@myhunter.cuny.edu>
2020-01-10 19:26:04 -0500

    switch tabs

--- src/app/main/users/users.component.ts
+++ src/app/main/users/users.component.ts
@@ -13,8 +13,27 @@ export class UsersComponent implements OnInit {
   ngOnInit() {
   }
 
+  doctors = false;
+  patients = false;
+  providers = false;
+
   switchTab(tabName) {
-    console.log(tabName);
+    if(tabName == "Doctors"){
+      this.doctors = true;
+      this.patients = false;
+      this.providers = false;
+    }
+    else if(tabName == "Patients"){
+      this.doctors = false;
+      this.patients = true;
+      this.providers = false;
+    }
+    else if(tabName == "Providers"){
+      this.doctors = false;
+      this.patients = false;
+      this.providers = true;
+    }
+
   }
 
 }
